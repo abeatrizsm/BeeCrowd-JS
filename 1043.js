d8a920bf6eb023a727067edfd1d9ebc9dd9e5c9b
Beatriz Mendes <anabeatriz.smendes@gmail.com>
2024-08-23 07:47:15 -0300

    1043

--- 1043.js
+++ 1043.js
@@ -1,1 +1,15 @@
 var prompt = function(texto) { return lines.shift();};
+
+var [a,b,c] = prompt("Insira 3 valores: ").split(" ");
+a = parseFloat(a);
+b = parseFloat(b);
+c = parseFloat(c);
+
+if(a<b+c && b<a+c && c<b+a){
+    var perimetro = a + b + c;
+    console.log("Perimetro = " + perimetro.toFixed(1));
+}
+else{
+    var area = ((a+b)*c)/2;
+    console.log("Area = " + area.toFixed(1));
+}
