e0c214e0eb16d51da6fda1a22dd090eda3f3bcf7
Beatriz Mendes <anabeatriz.smendes@gmail.com>
2024-08-23 07:51:59 -0300

    1044

--- 1044.js
+++ 1044.js
@@ -1,1 +1,12 @@
 var prompt = function(texto) { return lines.shift();};
+
+var [a,b] = prompt("Insira dois valores:").split(" ");
+a = parseInt(a);
+b = parseInt(b);
+
+if(a%b===0 || b%a===0){
+    console.log("Sao Multiplos");
+}
+else{
+    console.log("Nao sao Multiplos");
+}
