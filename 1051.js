162bffe374385c3e44756edfbf938d31cb832dc0
Beatriz Mendes <anabeatriz.smendes@gmail.com>
2024-08-24 11:48:14 -0300

    1051

--- 1051.js
+++ 1051.js
@@ -1,1 +1,20 @@
 var prompt = function(texto) { return lines.shift();};
+
+var salario = parseFloat(prompt("Insira o salário:"));
+var imposto = 0;
+
+if(salario<=2000){
+    console.log("Isento");
+}
+else{
+    if(salario>2000 && salario<=3000){
+        imposto = (salario - 2000) * 0.08;
+    }
+    else if(salario>3000 && salario<=4500){
+        imposto = 80 + ((salario - 3000) * 0.18);
+    }
+    else if(salario>4500){
+        imposto = 350 + ((salario - 4500) * 0.28);
+    }
+    console.log("R$ " + (imposto).toFixed(2));
+}
