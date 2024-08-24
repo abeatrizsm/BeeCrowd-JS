440eff065daeee813b7cef88e8c8f35ad4c00a29
Beatriz Mendes <anabeatriz.smendes@gmail.com>
2024-08-24 08:28:41 -0300

    1046

--- 1046.js
+++ 1046.js
@@ -1,1 +1,18 @@
 var prompt = function(texto) { return lines.shift();};
+
+var [inicio, final] = prompt("Insira a hora inicial e a final:").split(" ");
+inicio = parseInt(inicio);
+final = parseInt(final);
+var tempo = 0;
+
+if(inicio>final){
+    tempo = 24 - (inicio - final);
+}
+if(inicio<final){
+    tempo = final - inicio;
+}
+if(inicio===final){
+    tempo = 24;
+}
+console.log(`O JOGO DUROU ${tempo} HORA(S)`);
+
