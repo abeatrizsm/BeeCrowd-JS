9e4ca1aa87573e06b2987b9178a9e042176926f4
Beatriz Mendes <anabeatriz.smendes@gmail.com>
2024-09-18 09:15:34 -0300

    1067

--- 1067.js
+++ 1067.js
@@ -1,7 +1,9 @@
 var prompt = function(texto) { return lines.shift();};
 
-for(var i = 1; i<=100; i++){
-    if(i%2===0){
+let n = parseInt(prompt("Insira um valor: "));
+
+for(var i = 0; i <= n; i++){
+    if (i % 2 !== 0 ) {
         console.log(i);
     }
 }
