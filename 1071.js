95f86bd6dcd38d4b1b6cc8b33fc460150e72fe73
Beatriz Mendes <anabeatriz.smendes@gmail.com>
2024-09-18 10:30:02 -0300

    1071

--- 1071.js
+++ 1071.js
@@ -1,18 +1,19 @@
-let prompt = function(texto) { return lines.shift();};
+var prompt = function(texto) { return lines.shift();};
 
-let dentro = 0;
-let fora = 0;
-let n = parseInt(prompt("Insira o número de casos: "));
+let x = parseInt(prompt("Insira o primeiro valor: "));
+let y = parseInt(prompt("Insira o segundo valor: "));
+let soma = 0;
 
-for( let i = 1 ; i<=n; i++){
-    let x = parseInt(prompt("Insira o valor " + (i+1) + ": "));
-    if(x>=10 && x<=20){
-        dentro++;
-    }
-    else{
-        fora++;
-    }
+if(y>x){
+    x=y;
+    y=X;
 }
-console.log(dentro + " in");
-console.log(fora + " out");
+y++;
 
+while(x>y){
+   if(y%2!==0)
+   soma+=y;
+   y++;
+}
+
+console.log(soma);
