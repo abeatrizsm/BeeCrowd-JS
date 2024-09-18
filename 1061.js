50ed067b83a986c698cc8a7affe5a2891a82abc5
Beatriz Mendes <anabeatriz.smendes@gmail.com>
2024-09-18 08:51:10 -0300

    1061

--- 1061.js
+++ 1061.js
@@ -1,7 +1,39 @@
 var prompt = function(texto) { return lines.shift();};
 
-for(var i = 1; i<=100; i++){
-    if(i%2===0){
-        console.log(i);
-    }
+let [x, diaInicial] = prompt("").split(" ");
+let [horaInicial, minutoInicial, segundoInicial] = prompt("").split(" : ");
+let [y, diaFinal] = prompt("").split(" ");
+let [horaFinal, minutoFinal, segundoFinal] = prompt("").split(" : ");
+
+diaInicial = parseInt(diaInicial);
+diaFinal = parseInt(diaFinal);
+horaInicial = parseInt(horaInicial);
+horaFinal = parseInt(horaFinal);
+minutoInicial = parseInt(minutoInicial);
+minutoFinal = parseInt(minutoFinal);
+segundoInicial = parseInt(segundoInicial);
+segundoFinal = parseInt(segundoFinal);
+
+let dias = diaFinal - diaInicial;
+let horas = horaFinal - horaInicial;
+let minutos = minutoFinal - minutoInicial;
+let segundos = segundoFinal - segundoInicial;
+
+if (segundos < 0) {
+    segundos += 60;
+    minutos -= 1;
 }
+if (minutos < 0) {
+    minutos += 60;
+    horas -= 1;
+}
+if (horas < 0) {
+    horas += 24;
+    dias -= 1;
+}
+
+console.log(dias + " dia(s)");
+console.log(horas + " hora(s)");
+console.log(minutos + " minuto(s)");
+console.log(segundos + " segundo(s)");
+
