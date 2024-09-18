b512a870f1d89f229bf614c0b1872512d34638ba
Beatriz Mendes <anabeatriz.smendes@gmail.com>
2024-09-18 09:11:34 -0300

    1066

--- 1066.js
+++ 1066.js
@@ -1,7 +1,26 @@
 var prompt = function(texto) { return lines.shift();};
 
-for(var i = 1; i<=100; i++){
-    if(i%2===0){
-        console.log(i);
+let pares = 0;
+let impares = 0;
+let positivos = 0;
+let negativos = 0;
+
+for(var i = 0; i<5; i++){
+    let n = parseInt(prompt("Insira o valor " + (i+1) + ":"));
+    if(n % 2 === 0){
+        pares++;
+    }
+    else{
+        impares++;
+    }
+    if (n > 0) {
+        positivos++;
+    }
+    else if (n < 0) {
+        negativos++;
     }
 }
+console.log(pares + " valor(es) par(es)");
+console.log(impares + " valor(es) impar(es)");
+console.log(positivos+ " valor(es) positivo(s)");
+console.log(negativos + " valor(es) negativo(s)");
