b191dc172a46ce55794c0cfd3786a5980d2c38e0
Beatriz Mendes <anabeatriz.smendes@gmail.com>
2024-09-21 15:59:28 -0300

    1097

--- 1097.js
+++ 1097.js
@@ -1,2 +1,11 @@
 var prompt = function(texto) { return lines.shift();};
 
+let j = 7;
+
+for (let i = 1; i <= 9; i += 2) {
+    for (let k = 3; k >= 1; k--) {
+        console.log(`I=${i} J=${j}`);
+        j--;
+    }
+    j += 5;
+}
